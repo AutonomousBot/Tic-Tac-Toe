c7bceb9216420caa69f0ba44906590367b2a73af
fred <fredericnguyen777@gmail.com>
2022-02-09 20:55:47 -0500

    turn orders are now displayed. added addMark function. added global variables for players.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+// Module for the board data. Creates board DOM element.
 let gameBoard = (() => {
   let board = [];
-  // Sets board array with 9 empty elements.
-  board.length = 9;
+  const length = 9;
   const displayBoard = () => {
     // Creates a DOM element for every square of the board.
     const boardDOM = document.getElementById("board")
-    for (let i = 0; i < board.length; i++) {
+    for (let i = 0; i < length; i++) {
       const square = document.createElement("div");   
       square.setAttribute("class", "square");
       square.style.setProperty("flex-basis", `${100/3}%`)
@@ -15,64 +15,94 @@ let gameBoard = (() => {
   }
   return {
     board,
+    length,
     displayBoard
   }  
 })()
 
+// Displays the board.
+gameBoard.displayBoard()
+
 // Factory for players.
-const Player = (name, marker) => {
+const Player = (name, mark) => {
   let markedBoard = []
-  const getMarker = () => marker
+  const getMark = () => mark
   const getName = () => name
-  return {markedBoard, getMarker, getName}
+  return {markedBoard, getMark, getName}
 }
 
+// Adds event to twoPlayers button. Calls twoPlayerGame.
 const twoPlayers = document.getElementById("2players")
 twoPlayers.addEventListener("click", twoPlayerGame, false)
 
+// Declare global current player.
+let currentPlayer = ""
+// Declare players
+let player1 = {}
+let player2 = {}
+
 function twoPlayerGame() {
+  // Prompts for players' names and their marks.
   let player1Name = prompt("Enter player one's name (special characters excluded).")
   console.log(player1Name)
   while (player1Name == "" || !isAlphaNumeric(player1Name)) {
     player1Name = prompt("Enter player one's name (special characters excluded).")
   }
 
-  let player1Marker = prompt("Would you like to be X or O?").toUpperCase()
-  while (player1Marker != "X" && player1Marker != "O") {
-    player1Marker = prompt("Would you like to be X or O?").toUpperCase()
+  let player1Mark = prompt("Would you like to be X or O?").toUpperCase()
+  while (player1Mark != "X" && player1Mark != "O") {
+    player1Mark = prompt("Would you like to be X or O?").toUpperCase()
   }
 
   let player2Name = prompt("Enter player two's name (special characters excluded).")
   while (player2Name == "" || !isAlphaNumeric(player2Name)) {
     player2Name = prompt("Enter player two's name (special characters excluded).")
   }
-  let player2Marker = ""
-  player1Marker == "X"? player2Marker = "O" : player2Marker = "X"; 
+  let player2Mark = ""
+  player1Mark == "X"? player2Mark = "O" : player2Mark = "X"; 
 
-  const player1 = Player(player1Name, player1Marker)
-  const player2 = Player(player2Name, player2Marker)
-  
-  let currentPlayer = ""
-  // Determines who plays first and turn order.
-  const turn = (() => {
-    const turndisplay = document.getElementById("playerTurn")
-    for(let i = 0; i < board.length; i++) {
-      if (player1.markedBoard == [] && player2.markedBoard ==[])
-      {
-        // Randomizes first player.
-        (Math.floor(Math.random() * 2) + 1 == 1)? currentPlayer = player1 : currentPlayer = player2
-        turndisplay.textContent = `${currentPlayer.getName()} will go first`
-      }
-      else {
-        currentPlayer == player1? currentPlayer = player2 : currentPlayer = player1
-        turndisplay.textContent = `${currentPlayer.getName()} will go first`
-      }
-    }
-    return {currentPlayer}
-  })()
+  player1 = Player(player1Name, player1Mark)
+  player2 = Player(player2Name, player2Mark)
+
+  turn.displayTurn();
 }
 
+// Determines who plays first and turn order. Updates current player variable.
+let turn = (() => {
+  const displayTurn = () => {
+    if (!(Object.keys(player1).length === 0)) {
+      const turnDOM = document.getElementById("playerTurn")
+      for(let i = 0; i < gameBoard.length; i++) {
+        if (player1.markedBoard.length == 0 && player2.markedBoard.length == 0)
+        {
+          // Randomizes first player.
+          let first = Math.floor(Math.random() * 2) + 1;
+          (first == 1)? currentPlayer = player1 : currentPlayer = player2
+          turnDOM.textContent = `${currentPlayer.getName()} will go first`
+        }
+        else {
+          currentPlayer == player1? currentPlayer = player2 : currentPlayer = player1
+          turnDOM.textContent = `${currentPlayer.getName()} will go first`
+        }
+      }
+    }
+  }
+  return {currentPlayer, displayTurn}
+})()
 
+// 
+
+// Adds mark to board and edits each players current marks on the board.
+function addMark() {
+  if (this.textContent == "") {
+    const grid = this.id
+    // Updates board in gameBoard module
+    gameBoard.board[grid] = currentPlayer.mark
+    // Updates board DOM element.
+    document.getElementById(`{grid}`).textContent = currentPlayer.mark
+    currentPlayer.markedBoard.push(grid)
+  }
+}
 
 // Checks if input is aphanumeric.
 function isAlphaNumeric(str) {
@@ -86,6 +116,3 @@ function isAlphaNumeric(str) {
   }
   return true;
 }
-
-// Displays the board.
-gameBoard.displayBoard()
